a46b454ed1d73420464f1dec32e6d05c5c36f198
dinesh <dinesh@dinesh-laptop>
2008-08-13 15:17:27 +0530

    committing changes for web pages after update of create_tar_ball

--- web/uploader.php
+++ web/uploader.php
@@ -22,12 +22,10 @@ if($_FILES["uploadedfile"]["type"] == "application/x-gzip"
 		$tempfolder . $_FILES["uploadedfile"]["name"]);
 	
 	//extracting code
-	if($_FILES["uploadedfile"]["type"] == "application/zip") {
+	if($_FILES["uploadedfile"]["type"] == "application/zip")
 		exec('unzip '.$tempfolder.$_FILES["uploadedfile"]["name"].' -d '.$tempfolder, $op, $status);
-	}
-	else {
+	else
 		exec('tar -xf '.$tempfolder.$_FILES["uploadedfile"]["name"].' -C '.$tempfolder, $op, $status);
-	}
 	checkerror($status,"Error: cannot extract(tar error).");	
 
 	//if user not logged in
@@ -39,11 +37,13 @@ if($_FILES["uploadedfile"]["type"] == "application/x-gzip"
 		
 		//send mail for review to admins 
 		$subject = "Review: code upload at temporary repository"; 
-		$message = "Some developer has uploaded code who has not logged in.\n\nModule is stored in ".$temprepo.$folder.".\n\nOutput of ccanlint: \n";
+		$message = "Some developer has uploaded code who has not logged in.\n\nModule is stored in ".
+							$temprepo.$folder.".\n\nOutput of ccanlint: \n";
 			
     	$toaddress = getccanadmin($db);
    	mail($toaddress, $subject, $message, "From: $frommail");
-   	echo "<div align=\"center\"> Stored to temporary repository. Mail will be send to admin to get verification of the code.<//div>";
+   	echo "<div align=\"center\"> Stored to temporary repository. 
+   				Mail will be send to admin to get verification of the code.<//div>";
    	unlink($tempfolder.$_FILES["uploadedfile"]["name"]);
    	exit();
 	} 
@@ -73,14 +73,18 @@ if($_FILES["uploadedfile"]["type"] == "application/x-gzip"
 			// if owner is not same 
 			if(!(getowner($ccan_home_dir . $folder, $db) == $_SESSION['susername'])) {	
 				if(!file_exists($repopath . $ccan_home_dir . $folder . '-' . $_SESSION['susername']))   			
-	   			echo "<div align=\"center\">". $ccan_home_dir . $folder . " already exists. Renaming to " . $folder . "-" . $_SESSION['susername'] . "</div>";
+	   			echo "<div align=\"center\">". $ccan_home_dir . $folder . 
+	   					" already exists. Renaming to " . $folder . "-" . $_SESSION['susername'] . "</div>";
       		else
-	   			echo "<div align=\"center\">". $ccan_home_dir . $folder . "-" . $_SESSION['susername'] . " already exists. Overwriting " . $folder. "-" . $_SESSION['susername'] . "</div>";
+	   			echo "<div align=\"center\">". $ccan_home_dir . $folder . 
+	   					"-" . $_SESSION['susername'] . " already exists. Overwriting " . 
+	   						$folder. "-" . $_SESSION['susername'] . "</div>";
 	   		$rename = $folder."-".$_SESSION['susername'];
 	   	}
 	   	
 	   	else
-	   		echo "<div align=\"center\">".$repopath. $ccan_home_dir. $folder. " already exists(uploaded by you). Overwriting ". $repopath. $folder."</div>";
+	   		echo "<div align=\"center\">".$repopath. $ccan_home_dir. $folder.
+	   				 " already exists(uploaded by you). Overwriting ". $repopath. $folder."</div>";
 	   			
   		}
 
@@ -93,16 +97,23 @@ if($_FILES["uploadedfile"]["type"] == "application/x-gzip"
 	   rename($tempfolder . $folder, $exactpath . $ccan_home_dir . $rename);
    		
    	chdir($exactpath);
-		unset($op); exec($infotojson . $ccan_home_dir . $rename . " " . $ccan_home_dir. $rename."/_info.c ". $ccan_home_dir . $rename . "/json_" . $rename . " " . $_SESSION['susername']. " ../../" . $db, $op, $status);
+		unset($op); exec($infotojson . $ccan_home_dir . $rename . " " . $ccan_home_dir.
+						 $rename."/_info.c ". $ccan_home_dir . $rename . "/json_" . $rename . " " 
+						 	. $_SESSION['susername']. " ../../" . $db, $op, $status);
 		checkerror($status,"Error: In infotojson.");
 		
 		unset($op); exec('bzr add', $op, $status);
 		checkerror($status,"Error: bzr add error.");
 		
-		unset($op); exec('bzr commit --unchanged -m "commiting from ccan web ' . $rename . " " . $_SESSION['susername'] . '"', $op, $status);
+		unset($op); exec('bzr commit --unchanged -m "commiting from ccan web ' . $rename . 
+							" " . $_SESSION['susername'] . '"', $op, $status);
 		checkerror($status,"Error: bzr commit error.");	
 			
 		unset($op); exec($bzr_push, $op, $status);
+		checkerror($status,"Error: bzr push error.");
+		
+		unset($op); exec($create_dep_tar . " " . $ccan_home_dir. $rename . " ../../" . 
+							$tar_dir . " ../../" . $db , $op, $status); 
 		checkerror($status,"Error: bzr push error.");
 		
 		chdir('../..');
@@ -119,7 +130,9 @@ if($_FILES["uploadedfile"]["type"] == "application/x-gzip"
 		if($score == '')
 			$msg =  'Below is details for test.';
 			
-		echo "<div align=\"center\"><table><tr><td> Score for code is low. Cannot copy to repository. Moving to ". $junkcode.$folder.'-'.$_SESSION['susername']."... </br></br>". $msg ." </br></br></td></tr><tr><td>";
+		echo "<div align=\"center\"><table><tr><td> Score for code is low.
+				 Cannot copy to repository. Moving to ". $junkcode.$folder.'-'.
+				 	$_SESSION['susername']."... </br></br>". $msg ." </br></br></td></tr><tr><td>";
 
 		foreach($score as $disp)
 			echo "$disp</br>";
