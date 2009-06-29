e84f843dfeb798e66b32a212bb7ffb095eebd7aa
Rusty Russell <rusty@rustcorp.com.au>
2009-06-29 11:58:44 +0930

    zero-append test for tdb.

---
+++ ccan/tdb/test/run-zero-append.c
@@ -0,0 +1,37 @@
+#define _XOPEN_SOURCE 500
+#include "tdb/tdb.h"
+#include "tdb/io.c"
+#include "tdb/tdb.c"
+#include "tdb/lock.c"
+#include "tdb/freelist.c"
+#include "tdb/traverse.c"
+#include "tdb/transaction.c"
+#include "tdb/error.c"
+#include "tdb/open.c"
+#include "tap/tap.h"
+#include <stdlib.h>
+#include <err.h>
+
+int main(int argc, char *argv[])
+{
+	struct tdb_context *tdb;
+	TDB_DATA key, data;
+
+	plan_tests(4);
+	tdb = tdb_open(NULL, 1024, TDB_INTERNAL, O_CREAT|O_TRUNC|O_RDWR, 0600);
+	ok1(tdb);
+
+	/* Tickle bug on appending zero length buffer to zero length buffer. */
+	key.dsize = strlen("hi");
+	key.dptr = (void *)"hi";
+	data.dptr = (void *)"world";
+	data.dsize = 0;
+
+	ok1(tdb_append(tdb, key, data) == 0);
+	ok1(tdb_append(tdb, key, data) == 0);
+	data = tdb_fetch(tdb, key);
+	ok1(data.dsize == 0);
+	tdb_close(tdb);
+
+	return exit_status();
+}
