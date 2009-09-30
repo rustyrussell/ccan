517114490e7153643b4c0a12c59a060568d7ae00
Rusty Russell <rusty@rustcorp.com.au>
2009-09-30 12:47:22 +0930

    typesafe_cb: more support for typesafe compare functions.

--- ccan/typesafe_cb/typesafe_cb.h
+++ ccan/typesafe_cb/typesafe_cb.h
@@ -64,6 +64,28 @@ __builtin_choose_expr(__builtin_types_compatible_p(typeof(1?(expr):0), oktype), 
 		     rtype (*)(void *))
 
 /**
+ * typesafe_cb_const - cast a const callback function if it matches the arg
+ * @rtype: the return type of the callback function
+ * @fn: the callback function to cast
+ * @arg: the (pointer) argument to hand to the callback function.
+ *
+ * If a callback function takes a single argument, this macro does appropriate
+ * casts to a function which takes a single const void * argument if the
+ * callback provided matches the @arg.
+ *
+ * It is assumed that @arg is of pointer type: usually @arg is passed
+ * or assigned to a void * elsewhere anyway.
+ *
+ * Example:
+ *	void _register_callback(void (*fn)(const void *arg), const void *arg);
+ *	#define register_callback(fn, arg) \
+ *		_register_callback(typesafe_cb_const(void, (fn), (arg)), (arg))
+ */
+#define typesafe_cb_const(rtype, fn, arg)				\
+	cast_if_type((fn),						\
+		     rtype (*)(const typeof(*arg)*), rtype (*)(const void *))
+
+/**
  * typesafe_cb_preargs - cast a callback function if it matches the arg
  * @rtype: the return type of the callback function
  * @fn: the callback function to cast
@@ -117,4 +139,28 @@ __builtin_choose_expr(__builtin_types_compatible_p(typeof(1?(expr):0), oktype), 
 		     rtype (*)(typeof(arg), __VA_ARGS__),		\
 		     rtype (*)(void *, __VA_ARGS__))
 
+/**
+ * typesafe_cb_cmp - cast a compare function if it matches the arg
+ * @rtype: the return type of the callback function
+ * @fn: the callback function to cast
+ * @arg: the (pointer) argument(s) to hand to the compare function.
+ *
+ * If a callback function takes two matching-type arguments, this macro does
+ * appropriate casts to a function which takes two const void * arguments if
+ * the callback provided takes two a const pointers to @arg.
+ *
+ * It is assumed that @arg is of pointer type: usually @arg is passed
+ * or assigned to a void * elsewhere anyway.
+ *
+ * Example:
+ *	void _my_qsort(void *base, size_t nmemb, size_t size,
+ *		       int (*cmp)(const void *, const void *));
+ *	#define my_qsort(base, nmemb, cmpfn) \
+ *		_my_qsort((base), (nmemb), sizeof(*(base)), \
+ *			  typesafe_cb_cmp(int, (cmpfn), (base)), (arg))
+ */
+#define typesafe_cb_cmp(rtype, cmpfn, arg)				\
+	cast_if_type((cmpfn),						\
+		     rtype (*)(const typeof(*arg)*, const typeof(*arg)*), \
+		     rtype (*)(const void *, const void *))
 #endif /* CCAN_CAST_IF_TYPE_H */
