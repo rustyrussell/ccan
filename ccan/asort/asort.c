e96830aefad171590e1b803f90364894db0c76de
Rusty Russell <rusty@rustcorp.com.au>
2010-02-24 14:12:22 +1030

    asort: fix gcc warning.

--- ccan/asort/asort.c
+++ ccan/asort/asort.c
@@ -7,8 +7,8 @@ void _asort(void *base, size_t nmemb, size_t size,
 {
 #if HAVE_NESTED_FUNCTIONS
 	/* This gives bogus "warning: no previous prototype for ‘cmp’"
-	 * with gcc 4 with -Wmissing-prototypes.  But there's no way
-	 * to predeclare it, so we lose. */
+	 * with gcc 4 with -Wmissing-prototypes.  Hence the auto crap. */
+	auto int cmp(const void *a, const void *b);
 	int cmp(const void *a, const void *b)
 	{
 		return compar(a, b, ctx);
