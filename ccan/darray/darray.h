6ba4abebd2fdaba81a0dad774de3cd8fc99304dc
Joey Adams <joeyadams3.14159@gmail.com>
2011-02-19 05:36:14 -0500

    darray: Renamed array module to darray and made several improvements.
     * Removed talloc support.
     * Added a synopsis and rearranged macro definitions.
     * Switched allocation strategy from increments of 64 to powers of 2.
     * Replaced array_for and array_rof with the more useful
       and portable macros array_foreach and array_foreach_reverse.
     * Added typedefs array_* for common types.

---
+++ ccan/darray/darray.h
@@ -0,0 +1,353 @@
+/*
+        Copyright (c) 2009  Joseph A. Adams
+        All rights reserved.
+        
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions
+        are met:
+        1. Redistributions of source code must retain the above copyright
+           notice, this list of conditions and the following disclaimer.
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+        3. The name of the author may not be used to endorse or promote products
+           derived from this software without specific prior written permission.
+        
+        THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+        IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+        OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+        IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+        INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+        NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+        DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+        THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+        THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef CCAN_DARRAY_H
+#define CCAN_DARRAY_H
+
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+
+/*
+ * SYNOPSIS
+ *
+ * Life cycle of a darray (dynamically-allocated array):
+ *
+ *     darray(int) a = darray_new();
+ *     darray_free(a);
+ *
+ *     struct {darray(int) a;} foo;
+ *     darray_init(foo.a);
+ *     darray_free(foo.a);
+ *
+ * Typedefs for darrays of common types:
+ *
+ *     darray_char, darray_schar, darray_uchar
+ *     darray_short, darray_int, darray_long
+ *     darray_ushort, darray_uint, darray_ulong
+ *
+ * Access:
+ *
+ *     T      darray_item(darray(T) arr, size_t index);
+ *     size_t darray_size(darray(T) arr);
+ *     size_t darray_alloc(darray(T) arr);
+ *     bool   darray_empty(darray(T) arr);
+ *
+ * Insertion (single item):
+ *
+ *     void   darray_append(darray(T) arr, T item);
+ *     void   darray_prepend(darray(T) arr, T item);
+ *     void   darray_push(darray(T) arr, T item); // same as darray_append
+ *
+ * Insertion (multiple items):
+ *
+ *     void   darray_append_items(darray(T) arr, T *items, size_t count);
+ *     void   darray_prepend_items(darray(T) arr, T *items, size_t count);
+ *
+ *     void   darray_appends(darray(T) arr, [T item, [...]]);
+ *     void   darray_prepends(darray(T) arr, [T item, [...]]);
+ *
+ *     // Same functionality as above, but does not require typeof.
+ *     void   darray_appends_t(darray(T) arr, #T, [T item, [...]]);
+ *     void   darray_prepends_t(darray(T) arr, #T, [T item, [...]]);
+ *
+ * Removal:
+ *
+ *     T      darray_pop(darray(T) arr | darray_size(arr) != 0);
+ *     T*     darray_pop_check(darray(T*) arr);
+ *
+ * Replacement:
+ *
+ *     void   darray_from_items(darray(T) arr, T *items, size_t count);
+ *     void   darray_from_c(darray(T) arr, T c_array[N]);
+ *
+ * String buffer:
+ *
+ *     void   darray_append_string(darray(char) arr, const char *str);
+ *     void   darray_append_lit(darray(char) arr, char stringLiteral[N+1]);
+ *
+ *     void   darray_prepend_string(darray(char) arr, const char *str);
+ *     void   darray_prepend_lit(darray(char) arr, char stringLiteral[N+1]);
+ *
+ *     void   darray_from_string(darray(T) arr, const char *str);
+ *     void   darray_from_lit(darray(char) arr, char stringLiteral[N+1]);
+ *
+ * Size management:
+ *
+ *     void   darray_resize(darray(T) arr, size_t newSize);
+ *     void   darray_resize0(darray(T) arr, size_t newSize);
+ *
+ *     void   darray_realloc(darray(T) arr, size_t newAlloc);
+ *     void   darray_growalloc(darray(T) arr, size_t newAlloc);
+ *
+ *     void   darray_make_room(darray(T) arr, size_t room);
+ *
+ * Traversal:
+ *
+ *     darray_foreach(T *&i, darray(T) arr) {...}
+ *     darray_foreach_reverse(T *&i, darray(T) arr) {...}
+ *
+ * Except for darray_foreach and darray_foreach_reverse,
+ * all macros evaluate their non-darray arguments only once.
+ */
+
+/*** Life cycle ***/
+
+#define darray(type) struct {type *item; size_t size; size_t alloc;}
+
+#define darray_new() {0,0,0}
+#define darray_init(arr) do {(arr).item=0; (arr).size=0; (arr).alloc=0;} while(0)
+#define darray_free(arr) do {free((arr).item);} while(0)
+
+
+/*
+ * Typedefs for darrays of common types.  These are useful
+ * when you want to pass a pointer to an darray(T) around.
+ *
+ * The following will produce an incompatible pointer warning:
+ *
+ *     void foo(darray(int) *arr);
+ *     darray(int) arr = darray_new();
+ *     foo(&arr);
+ *
+ * The workaround:
+ *
+ *     void foo(darray_int *arr);
+ *     darray_int arr = darray_new();
+ *     foo(&arr);
+ */
+
+typedef darray(char)           darray_char;
+typedef darray(signed char)    darray_schar;
+typedef darray(unsigned char)  darray_uchar;
+
+typedef darray(short)          darray_short;
+typedef darray(int)            darray_int;
+typedef darray(long)           darray_long;
+
+typedef darray(unsigned short) darray_ushort;
+typedef darray(unsigned int)   darray_uint;
+typedef darray(unsigned long)  darray_ulong;
+
+
+/*** Access ***/
+
+#define darray_item(arr, i) ((arr).item[i])
+#define darray_size(arr)    ((arr).size)
+#define darray_alloc(arr)   ((arr).alloc)
+#define darray_empty(arr)   ((arr).size == 0)
+
+
+/*** Insertion (single item) ***/
+
+#define darray_append(arr, ...) do { \
+		darray_resize(arr, (arr).size+1); \
+		(arr).item[(arr).size-1] = (__VA_ARGS__); \
+	} while(0)
+#define darray_prepend(arr, ...) do { \
+		darray_resize(arr, (arr).size+1); \
+		memmove((arr).item+1, (arr).item, ((arr).size-1)*sizeof(*(arr).item)); \
+		(arr).item[0] = (__VA_ARGS__); \
+	} while(0)
+#define darray_push(arr, ...) darray_append(arr, __VA_ARGS__)
+
+
+/*** Insertion (multiple items) ***/
+
+#define darray_append_items(arr, items, count) do { \
+		size_t __count = (count), __oldSize = (arr).size; \
+		darray_resize(arr, __oldSize + __count); \
+		memcpy((arr).item + __oldSize, items, __count * sizeof(*(arr).item)); \
+	} while(0)
+
+#define darray_prepend_items(arr, items, count) do { \
+		size_t __count = (count), __oldSize = (arr).size; \
+		darray_resize(arr, __count + __oldSize); \
+		memmove((arr).item + __count, (arr).item, __oldSize * sizeof(*(arr).item)); \
+		memcpy((arr).item, items, __count * sizeof(*(arr).item)); \
+	} while(0)
+
+#define darray_append_items_nullterminate(arr, items, count) do { \
+		size_t __count = (count), __oldSize = (arr).size; \
+		darray_resize(arr, __oldSize + __count + 1); \
+		memcpy((arr).item + __oldSize, items, __count * sizeof(*(arr).item)); \
+		(arr).item[--(arr).size] = 0; \
+	} while(0)
+
+#define darray_prepend_items_nullterminate(arr, items, count) do { \
+		size_t __count = (count), __oldSize = (arr).size; \
+		darray_resize(arr, __count + __oldSize + 1); \
+		memmove((arr).item + __count, (arr).item, __oldSize * sizeof(*(arr).item)); \
+		memcpy((arr).item, items, __count * sizeof(*(arr).item)); \
+		(arr).item[--(arr).size] = 0; \
+	} while(0)
+
+#if HAVE_TYPEOF
+#define darray_appends(arr, ...) darray_appends_t(arr, typeof((*(arr).item)), __VA_ARGS__)
+#define darray_prepends(arr, ...) darray_prepends_t(arr, typeof((*(arr).item)), __VA_ARGS__)
+#endif
+
+#define darray_appends_t(arr, type, ...) do { \
+		type __src[] = {__VA_ARGS__}; \
+		darray_append_items(arr, __src, sizeof(__src)/sizeof(*__src)); \
+	} while(0)
+#define darray_prepends_t(arr, type, ...) do { \
+		type __src[] = {__VA_ARGS__}; \
+		darray_prepend_items(arr, __src, sizeof(__src)/sizeof(*__src)); \
+	} while(0)
+
+
+/*** Removal ***/
+
+/* Warning: Do not call darray_pop on an empty darray. */
+#define darray_pop(arr) ((arr).item[--(arr).size])
+#define darray_pop_check(arr) ((arr).size ? darray_pop(arr) : NULL)
+
+
+/*** Replacement ***/
+
+#define darray_from_items(arr, items, count) do {size_t __count = (count); darray_resize(arr, __count); memcpy((arr).item, items, __count*sizeof(*(arr).item));} while(0)
+#define darray_from_c(arr, c_array) darray_from_items(arr, c_array, sizeof(c_array)/sizeof(*(c_array)))
+
+
+/*** String buffer ***/
+
+#define darray_append_string(arr, str) do {const char *__str = (str); darray_append_items(arr, __str, strlen(__str)+1); (arr).size--;} while(0)
+#define darray_append_lit(arr, stringLiteral) do {darray_append_items(arr, stringLiteral, sizeof(stringLiteral)); (arr).size--;} while(0)
+
+#define darray_prepend_string(arr, str) do { \
+		const char *__str = (str); \
+		darray_prepend_items_nullterminate(arr, __str, strlen(__str)); \
+	} while(0)
+#define darray_prepend_lit(arr, stringLiteral) \
+	darray_prepend_items_nullterminate(arr, stringLiteral, sizeof(stringLiteral) - 1)
+
+#define darray_from_string(arr, str) do {const char *__str = (str); darray_from_items(arr, __str, strlen(__str)+1); (arr).size--;} while(0)
+#define darray_from_lit(arr, stringLiteral) do {darray_from_items(arr, stringLiteral, sizeof(stringLiteral)); (arr).size--;} while(0)
+
+
+/*** Size management ***/
+
+#define darray_resize(arr, newSize) darray_growalloc(arr, (arr).size = (newSize))
+#define darray_resize0(arr, newSize) do { \
+		size_t __oldSize = (arr).size, __newSize = (newSize); \
+		(arr).size = __newSize; \
+		if (__newSize > __oldSize) { \
+			darray_growalloc(arr, __newSize); \
+			memset(&(arr).item[__oldSize], 0, (__newSize - __oldSize) * sizeof(*(arr).item)); \
+		} \
+	} while(0)
+
+#define darray_realloc(arr, newAlloc) do { \
+		(arr).item = realloc((arr).item, ((arr).alloc = (newAlloc)) * sizeof(*(arr).item)); \
+	} while(0)
+#define darray_growalloc(arr, need) do { \
+		size_t __need = (need); \
+		if (__need > (arr).alloc) \
+			darray_realloc(arr, darray_next_alloc((arr).alloc, __need)); \
+	} while(0)
+
+#if HAVE_STATEMENT_EXPR==1
+#define darray_make_room(arr, room) ({size_t newAlloc = (arr).size+(room); if ((arr).alloc<newAlloc) darray_realloc(arr, newAlloc); (arr).item+(arr).size; })
+#endif
+
+static inline size_t darray_next_alloc(size_t alloc, size_t need)
+{
+	if (alloc == 0)
+		alloc = 1;
+	while (alloc < need)
+		alloc *= 2;
+	return alloc;
+}
+
+
+/*** Traversal ***/
+
+/*
+ * darray_foreach(T *&i, darray(T) arr) {...}
+ *
+ * Traverse a darray.  `i` must be declared in advance as a pointer to an item.
+ */
+#define darray_foreach(i, arr) \
+	for ((i) = &(arr).item[0]; (i) < &(arr).item[(arr).size]; (i)++)
+
+/*
+ * darray_foreach_reverse(T *&i, darray(T) arr) {...}
+ *
+ * Like darray_foreach, but traverse in reverse order.
+ */
+#define darray_foreach_reverse(i, arr) \
+	for ((i) = &(arr).item[(arr).size]; (i)-- > &(arr).item[0]; )
+
+
+#endif /* CCAN_DARRAY_H */
+
+/*
+
+darray_growalloc(arr, newAlloc) sees if the darray can currently hold newAlloc items;
+	if not, it increases the alloc to satisfy this requirement, allocating slack
+	space to avoid having to reallocate for every size increment.
+
+darray_from_string(arr, str) copies a string to an darray_char.
+
+darray_push(arr, item) pushes an item to the end of the darray.
+darray_pop(arr) pops it back out.  Be sure there is at least one item in the darray before calling.
+darray_pop_check(arr) does the same as darray_pop, but returns NULL if there are no more items left in the darray.
+
+darray_make_room(arr, room) ensures there's 'room' elements of space after the end of the darray, and it returns a pointer to this space.
+Currently requires HAVE_STATEMENT_EXPR, but I plan to remove this dependency by creating an inline function.
+
+The following require HAVE_TYPEOF==1 :
+
+darray_appends(arr, item0, item1...) appends a collection of comma-delimited items to the darray.
+darray_prepends(arr, item0, item1...) prepends a collection of comma-delimited items to the darray.\
+
+
+Examples:
+
+	darray(int)  arr;
+	int        *i;
+	
+	darray_appends(arr, 0,1,2,3,4);
+	darray_appends(arr, -5,-4,-3,-2,-1);
+	darray_foreach(i, arr)
+		printf("%d ", *i);
+	printf("\n");
+	
+	darray_free(arr);
+	
+
+	typedef struct {int n,d;} Fraction;
+	darray(Fraction) fractions;
+	Fraction        *i;
+	
+	darray_appends(fractions, {3,4}, {3,5}, {2,1});
+	darray_foreach(i, fractions)
+		printf("%d/%d\n", i->n, i->d);
+	
+	darray_free(fractions);
+*/
---
+++ ccan/darray/test/run.c
@@ -0,0 +1,349 @@
+#include <ccan/tap/tap.h>
+#include <ccan/darray/darray.h>
+#include <stdio.h>
+
+#include "lotsOfNumbers.h"
+#include "lotsOfStrings.h"
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(*(arr)))
+
+struct {
+	char *stringsF, *stringsB;
+		//items of lotsOfStrings glued together
+	size_t stringsSize; //total strlen of all strings combined
+} amalgams;
+
+static void generateAmalgams(void);
+static void freeAmalgams(void);
+static int isZeros(void *ptr, size_t size);
+static void memtile(void *dest, size_t destWidth, const void *src, size_t srcWidth);
+
+
+#if 0
+#define testing(...) printf("Testing %s...\n", #__VA_ARGS__)
+#define trace(...) do {printf(__VA_ARGS__); puts("");} while(0)
+#else
+#define testing(...) do {} while(0)
+#define trace(...) do {} while(0)
+#endif
+
+#include "testLits.h"
+
+int main(void) {
+	darray(long) arr = darray_new();
+	darray_char str = darray_new();
+	#define reset(arr) do {darray_free(arr); darray_init(arr);} while(0)
+	size_t i;
+	
+	trace("Generating amalgams (internal)");
+	generateAmalgams();
+	
+	plan_tests(41);
+	
+	testLits();
+	
+	testing(darray_push);
+	{
+		for (i=0; i < ARRAY_SIZE(lotsOfNumbers); i++)
+			darray_push(arr, lotsOfNumbers[i]);
+		ok1(darray_size(arr) == ARRAY_SIZE(lotsOfNumbers));
+		ok1(darray_alloc(arr) >= darray_size(arr));
+		ok1(!memcmp(arr.item, lotsOfNumbers, sizeof(lotsOfNumbers)));
+	}
+	reset(arr);
+	
+	testing(darray_prepend, darray_pop);
+	{
+		for (i = ARRAY_SIZE(lotsOfNumbers); i;)
+			darray_prepend(arr, lotsOfNumbers[--i]);
+		ok1(darray_size(arr) == ARRAY_SIZE(lotsOfNumbers));
+		ok1(darray_alloc(arr) >= darray_size(arr));
+		ok1(!memcmp(arr.item, lotsOfNumbers, sizeof(lotsOfNumbers)));
+		
+		for (i = ARRAY_SIZE(lotsOfNumbers); i;) {
+			if (darray_pop(arr) != (long)lotsOfNumbers[--i]) {
+				i++;
+				break;
+			}
+		}
+		ok1(i==0);
+		ok1(darray_size(arr) == 0);
+	}
+	reset(arr);
+	
+	testing(darray_from_c, darray_foreach, darray_foreach_reverse);
+	{
+		long   *i;
+		size_t  j;
+		
+		darray_from_c(arr, lotsOfNumbers);
+		ok1(darray_size(arr) == ARRAY_SIZE(lotsOfNumbers));
+		ok1(darray_alloc(arr) >= darray_size(arr));
+		ok1(memcmp(arr.item, lotsOfNumbers, sizeof(lotsOfNumbers)) == 0);
+		
+		j = 0;
+		darray_foreach(i, arr) {
+			if (i - arr.item != j)
+				break;
+			if (*i != (long)lotsOfNumbers[j])
+				break;
+			j++;
+		};
+		ok1(j == ARRAY_SIZE(lotsOfNumbers));
+		
+		j = 0;
+		darray_foreach_reverse(i, arr) {
+			if (i - arr.item != darray_size(arr)-j-1)
+				break;
+			if (*i != (long)lotsOfNumbers[darray_size(arr)-j-1])
+				break;
+			j++;
+		};
+		ok1(j == ARRAY_SIZE(lotsOfNumbers));
+	}
+	reset(arr);
+	
+	testing(darray_append_string);
+	{
+		for (i=0; i < ARRAY_SIZE(lotsOfStrings); i++)
+			darray_append_string(str, lotsOfStrings[i]);
+		ok1(str.size == amalgams.stringsSize);
+		ok1(str.alloc > str.size);
+		ok1(str.item[str.size] == 0);
+		ok1(!strcmp(str.item, amalgams.stringsF));
+	}
+	reset(str);
+	
+	testing(darray_prepend_string);
+	{
+		for (i=0; i < ARRAY_SIZE(lotsOfStrings); i++)
+			darray_prepend_string(str, lotsOfStrings[i]);
+		ok1(str.size == amalgams.stringsSize);
+		ok1(str.alloc > str.size);
+		ok1(str.item[str.size] == 0);
+		ok1(!strcmp(str.item, amalgams.stringsB));
+	}
+	reset(str);
+	
+	testing(darray_from_string);
+	{
+		for (i=0; i < ARRAY_SIZE(lotsOfStrings); i++) {
+			darray_from_string(str, lotsOfStrings[i]);
+			if (str.size != strlen(lotsOfStrings[i]))
+				break;
+			if (str.alloc < strlen(lotsOfStrings[i])+1)
+				break;
+			if (strcmp(str.item, lotsOfStrings[i]))
+				break;
+		}
+		ok1(i == ARRAY_SIZE(lotsOfStrings));
+	}
+	reset(str);
+	
+	testing(darray_resize0);
+	{
+		size_t prevSize=0, size;
+		for (i=0; i < ARRAY_SIZE(lotsOfNumbers); i++, prevSize=size) {
+			size = lotsOfNumbers[i] & 0xFFFF;
+			darray_resize0(arr, size);
+			if (darray_size(arr) != size)
+				break;
+			if (darray_alloc(arr) < size)
+				break;
+			if (size>prevSize) {
+				if (!isZeros(arr.item+prevSize, (size-prevSize)*sizeof(*arr.item)))
+					break;
+			}
+			//fill the darray with lotsOfNumbers garbage
+			memtile(arr.item, darray_size(arr)*sizeof(*arr.item), lotsOfNumbers, sizeof(lotsOfNumbers));
+		}
+		ok1(i == ARRAY_SIZE(lotsOfNumbers));
+	}
+	reset(arr);
+	
+	testing(darray_realloc);
+	{
+		size_t s,a;
+		for (i=0; i < ARRAY_SIZE(lotsOfNumbers); i++) {
+			arr.size = (s = lotsOfNumbers[i] >> 16);
+				//give size a nonsense value to make sure darray_realloc doesn't care about it
+			a = amalgams.stringsSize/sizeof(*arr.item)+2;
+			darray_realloc(arr, a = lotsOfNumbers[i] % ((amalgams.stringsSize/sizeof(*arr.item))+1));
+			if (a*sizeof(*arr.item) > amalgams.stringsSize)
+				break;
+			if (darray_alloc(arr) != a)
+				break;
+			if (darray_size(arr) != s)
+				break;
+			memtile(arr.item, a*sizeof(*arr.item), amalgams.stringsF, a*sizeof(*arr.item));
+			if (memcmp(arr.item, amalgams.stringsF, a*sizeof(*arr.item)))
+				break;
+		}
+		ok1(i == ARRAY_SIZE(lotsOfNumbers));
+	}
+	reset(arr);
+	
+	testing(darray_growalloc);
+	{
+		size_t prevA, s, a;
+		for (i=0; i < ARRAY_SIZE(lotsOfNumbers); i++) {
+			arr.size = (s = lotsOfNumbers[i] >> 16);
+				//give size a nonsense value to make sure darray_growalloc doesn't care about it
+			a = amalgams.stringsSize/sizeof(*arr.item)+2;
+			prevA = darray_alloc(arr);
+			darray_growalloc(arr, a = lotsOfNumbers[i] % ((amalgams.stringsSize/sizeof(*arr.item))+1));
+			if (a*sizeof(*arr.item) > amalgams.stringsSize)
+				break;
+			if (darray_alloc(arr) < a)
+				break;
+			if (darray_alloc(arr) < prevA)
+				break;
+			if (darray_size(arr) != s)
+				break;
+			
+			memtile(arr.item, a*sizeof(*arr.item), amalgams.stringsF, a*sizeof(*arr.item));
+			if (memcmp(arr.item, amalgams.stringsF, a*sizeof(*arr.item)))
+				break;
+			
+			//clear the darray every now and then
+			if (!(lotsOfNumbers[i] & 15)) {
+				reset(arr);
+			}
+		}
+		ok1(i == ARRAY_SIZE(lotsOfNumbers));
+	}
+	reset(arr);
+	
+	testing(darray_make_room);
+	{
+		for (i=0; i < ARRAY_SIZE(lotsOfStrings); i++) {
+			char *dest = darray_make_room(str, strlen(lotsOfStrings[i]));
+			if (str.alloc < str.size+strlen(lotsOfStrings[i]))
+				break;
+			if (dest != str.item+str.size)
+				break;
+			
+			memcpy(dest, lotsOfStrings[i], strlen(lotsOfStrings[i]));
+			str.size += strlen(lotsOfStrings[i]);
+		}
+		ok1(i == ARRAY_SIZE(lotsOfStrings));
+		ok1(str.size == amalgams.stringsSize);
+		
+		darray_append(str, 0);
+		ok1(!strcmp(str.item, amalgams.stringsF));
+	}
+	reset(str);
+	
+	testing(darray_appends, darray_prepends, darray_pop_check);
+	{
+		darray(const char*) arr = darray_new();
+		const char *n[9] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight"};
+		
+		darray_appends(arr, n[5], n[6], n[7], n[8]);
+		ok1(darray_size(arr)==4 && darray_alloc(arr)>=4);
+		
+		darray_prepends(arr, n[0], n[1], n[2], n[3], n[4]);
+		ok1(darray_size(arr)==9 && darray_alloc(arr)>=9);
+		
+		ok1(arr.item[0]==n[0] &&
+		    arr.item[1]==n[1] &&
+		    arr.item[2]==n[2] &&
+		    arr.item[3]==n[3] &&
+		    arr.item[4]==n[4] &&
+		    arr.item[5]==n[5] &&
+		    arr.item[6]==n[6] &&
+		    arr.item[7]==n[7] &&
+		    arr.item[8]==n[8]);
+		
+		ok1(darray_pop_check(arr)==n[8] &&
+		    darray_pop_check(arr)==n[7] &&
+		    darray_pop_check(arr)==n[6] &&
+		    darray_pop_check(arr)==n[5] &&
+		    darray_pop_check(arr)==n[4] &&
+		    darray_pop_check(arr)==n[3] &&
+		    darray_pop_check(arr)==n[2] &&
+		    darray_pop_check(arr)==n[1] &&
+		    darray_pop_check(arr)==n[0]);
+		
+		ok1(darray_size(arr)==0);
+		
+		ok1(darray_pop_check(arr)==NULL && darray_pop_check(arr)==NULL && darray_pop_check(arr)==NULL);
+		
+		darray_free(arr);
+	}
+	
+	trace("Freeing amalgams (internal)");
+	freeAmalgams();
+	
+	return exit_status();
+}
+
+static void generateAmalgams(void) {
+	size_t i;
+	size_t lotsOfStringsLen = 0;
+	const char *src;
+	char *p;
+	
+	for (i=0; i < ARRAY_SIZE(lotsOfStrings); i++)
+		lotsOfStringsLen += strlen(lotsOfStrings[i]);
+	amalgams.stringsSize = lotsOfStringsLen;
+	
+	amalgams.stringsF = malloc(lotsOfStringsLen+1);
+	amalgams.stringsB = malloc(lotsOfStringsLen+1);
+	
+	for (i=0,p=amalgams.stringsF; i < ARRAY_SIZE(lotsOfStrings); i++) {
+		size_t len = strlen(src=lotsOfStrings[i]);
+		memcpy(p, src, len);
+		p += len;
+	}
+	*p = 0;
+	ok1(p-amalgams.stringsF == (long)lotsOfStringsLen);
+	ok1(strlen(amalgams.stringsF) == lotsOfStringsLen);
+	
+	for (i = ARRAY_SIZE(lotsOfStrings), p = amalgams.stringsB; i--;) {
+		size_t len = strlen(src=lotsOfStrings[i]);
+		memcpy(p, src, len);
+		p += len;
+	}
+	*p = 0;
+	ok1(p-amalgams.stringsB == (long)lotsOfStringsLen);
+	ok1(strlen(amalgams.stringsB) == lotsOfStringsLen);
+}
+
+static void freeAmalgams(void) {
+	free(amalgams.stringsF);
+	free(amalgams.stringsB);
+}
+
+static int isZeros(void *ptr, size_t size) {
+	unsigned char *pc = ptr;
+	size_t *pl;
+	if (size>8) {
+		//test one byte at a time until we have an aligned size_t pointer
+		while ((size_t)pc & (sizeof(size_t)-1))
+			if (*pc++)
+				return 0;
+		pl = (size_t*)pc;
+		size -= pc-(unsigned char*)ptr;
+		while (size >= sizeof(size_t)) {
+			size -= sizeof(size_t);
+			if (*pl++)
+				return 0;
+		}
+		pc = (unsigned char*)pl;
+	}
+	while (size--)
+		if (*pc++)
+			return 0;
+	return 1;
+}
+
+static void memtile(void *dest, size_t destWidth, const void *src, size_t srcWidth) {
+	char *d = dest;
+	while (destWidth > srcWidth) {
+		destWidth -= srcWidth;
+		memcpy(d, src, srcWidth);
+		d += srcWidth;
+	}
+	memcpy(d, src, destWidth);
+}
