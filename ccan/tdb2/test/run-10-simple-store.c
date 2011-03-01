be1d6120bebb5bedf6d77b2e23ae3a850d3177e3
Rusty Russell <rusty@rustcorp.com.au>
2011-03-01 23:19:18 +1030

    tdb2: failtest on tdb_store
    Increase from:
    	1985 of 2553 lines covered
    to:
    	2018 of 2552 lines covered

--- ccan/tdb2/test/run-10-simple-store.c
+++ ccan/tdb2/test/run-10-simple-store.c
@@ -1,3 +1,4 @@
+#include <ccan/failtest/failtest_override.h>
 #include <ccan/tdb2/tdb.c>
 #include <ccan/tdb2/free.c>
 #include <ccan/tdb2/lock.c>
@@ -6,7 +7,20 @@
 #include <ccan/tdb2/transaction.c>
 #include <ccan/tdb2/check.c>
 #include <ccan/tap/tap.h>
+#include <ccan/failtest/failtest.h>
 #include "logging.h"
+#include "failtest_helper.h"
+
+static bool failtest_suppress = false;
+
+/* Don't need to test everything here, just want expand testing. */
+static enum failtest_result
+suppress_failure(struct failtest_call *history, unsigned num)
+{
+	if (failtest_suppress)
+		return FAIL_DONT_FAIL;
+	return block_repeat_failures(history, num);
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,26 +32,44 @@ int main(int argc, char *argv[])
 	struct tdb_data key = { (unsigned char *)"key", 3 };
 	struct tdb_data data = { (unsigned char *)"data", 4 };
 
+	failtest_init(argc, argv);
+	failtest_hook = suppress_failure;
+	failtest_exit_check = exit_check_log;
+
+	failtest_suppress = true;
 	plan_tests(sizeof(flags) / sizeof(flags[0]) * 7 + 1);
 	for (i = 0; i < sizeof(flags) / sizeof(flags[0]); i++) {
 		tdb = tdb_open("run-10-simple-store.tdb", flags[i],
 			       O_RDWR|O_CREAT|O_TRUNC, 0600, &tap_log_attr);
-		ok1(tdb);
-		if (tdb) {
-			/* Modify should fail. */
-			ok1(tdb_store(tdb, key, data, TDB_MODIFY)
-			    == TDB_ERR_NOEXIST);
-			ok1(tdb_check(tdb, NULL, NULL) == 0);
-			/* Insert should succeed. */
-			ok1(tdb_store(tdb, key, data, TDB_INSERT) == 0);
-			ok1(tdb_check(tdb, NULL, NULL) == 0);
-			/* Second insert should fail. */
-			ok1(tdb_store(tdb, key, data, TDB_INSERT)
-			    == TDB_ERR_EXISTS);
-			ok1(tdb_check(tdb, NULL, NULL) == 0);
-			tdb_close(tdb);
-		}
+		if (!ok1(tdb))
+			break;
+		/* Modify should fail. */
+		failtest_suppress = false;
+		if (!ok1(tdb_store(tdb, key, data, TDB_MODIFY)
+			 == TDB_ERR_NOEXIST))
+			goto fail;
+		failtest_suppress = true;
+		ok1(tdb_check(tdb, NULL, NULL) == 0);
+		/* Insert should succeed. */
+		failtest_suppress = false;
+		if (!ok1(tdb_store(tdb, key, data, TDB_INSERT) == 0))
+			goto fail;
+		failtest_suppress = true;
+		ok1(tdb_check(tdb, NULL, NULL) == 0);
+		/* Second insert should fail. */
+		failtest_suppress = false;
+		if (!ok1(tdb_store(tdb, key, data, TDB_INSERT)
+			 == TDB_ERR_EXISTS))
+			goto fail;
+		failtest_suppress = true;
+		ok1(tdb_check(tdb, NULL, NULL) == 0);
+		tdb_close(tdb);
 	}
 	ok1(tap_log_messages == 0);
-	return exit_status();
+	failtest_exit(exit_status());
+
+fail:
+	failtest_suppress = true;
+	tdb_close(tdb);
+	failtest_exit(exit_status());
 }
