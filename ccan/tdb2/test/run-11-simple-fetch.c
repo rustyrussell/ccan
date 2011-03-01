399c393dd31e7430adbaf9526fc95513e0e0e1fb
Rusty Russell <rusty@rustcorp.com.au>
2011-03-01 23:19:18 +1030

    tdb2: failtest on tdb_fetch
    Increase from:
    	1985 of 2553 lines covered
    to:
    	2020 of 2552 lines covered

--- ccan/tdb2/test/run-11-simple-fetch.c
+++ ccan/tdb2/test/run-11-simple-fetch.c
@@ -1,3 +1,4 @@
+#include <ccan/failtest/failtest_override.h>
 #include <ccan/tdb2/tdb.c>
 #include <ccan/tdb2/free.c>
 #include <ccan/tdb2/lock.c>
@@ -6,7 +7,20 @@
 #include <ccan/tdb2/transaction.c>
 #include <ccan/tdb2/check.c>
 #include <ccan/tap/tap.h>
+#include <ccan/failtest/failtest.h>
 #include "logging.h"
+#include "failtest_helper.h"
+
+static bool failtest_suppress = false;
+
+/* Don't need to test everything here, just want fetch testing. */
+static enum failtest_result
+suppress_failure(struct failtest_call *history, unsigned num)
+{
+	if (failtest_suppress)
+		return FAIL_DONT_FAIL;
+	return block_repeat_failures(history, num);
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +32,11 @@ int main(int argc, char *argv[])
 	struct tdb_data key = { (unsigned char *)"key", 3 };
 	struct tdb_data data = { (unsigned char *)"data", 4 };
 
+	failtest_init(argc, argv);
+	failtest_hook = suppress_failure;
+	failtest_exit_check = exit_check_log;
+
+	failtest_suppress = true;
 	plan_tests(sizeof(flags) / sizeof(flags[0]) * 8 + 1);
 	for (i = 0; i < sizeof(flags) / sizeof(flags[0]); i++) {
 		tdb = tdb_open("run-11-simple-fetch.tdb", flags[i],
@@ -27,13 +46,19 @@ int main(int argc, char *argv[])
 			struct tdb_data d;
 
 			/* fetch should fail. */
-			ok1(tdb_fetch(tdb, key, &d) == TDB_ERR_NOEXIST);
+			failtest_suppress = false;
+			if (!ok1(tdb_fetch(tdb, key, &d) == TDB_ERR_NOEXIST))
+				goto fail;
+			failtest_suppress = true;
 			ok1(tdb_check(tdb, NULL, NULL) == 0);
 			/* Insert should succeed. */
 			ok1(tdb_store(tdb, key, data, TDB_INSERT) == 0);
 			ok1(tdb_check(tdb, NULL, NULL) == 0);
 			/* Fetch should now work. */
-			ok1(tdb_fetch(tdb, key, &d) == TDB_SUCCESS);
+			failtest_suppress = false;
+			if (!ok1(tdb_fetch(tdb, key, &d) == TDB_SUCCESS))
+				goto fail;
+			failtest_suppress = true;
 			ok1(data_equal(d, data));
 			free(d.dptr);
 			ok1(tdb_check(tdb, NULL, NULL) == 0);
@@ -42,4 +67,9 @@ int main(int argc, char *argv[])
 	}
 	ok1(tap_log_messages == 0);
 	return exit_status();
+
+fail:
+	failtest_suppress = true;
+	tdb_close(tdb);
+	failtest_exit(exit_status());
 }
