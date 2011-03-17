ecea073699c60fd5aab9e3955f59a7042d637b68
Rusty Russell <rusty@rustcorp.com.au>
2011-03-17 22:12:22 +1030

    str: provide checks for ctype.h char functions, and strstr and strchr functions.
    In the former case, we were bitten by the fact that you don't pass a char
    to isalpha() et al: you pass an int.  This means on most platforms you want
    to do:
    
       if (isalpha((unsigned char)c)) ...
    
    Insane?  Yes, but I assure you I'm not making this up.
    
    Similarly, I've always wanted strstr, strchr and strrchr to return
    const char * when given a const char * argument to search, to avoid
    constness leak.
    
    In both cases, the actual versions from the headers may be macros, so
    we need to be very careful overriding them.  The result is that they
    become out-of-line functions which is unacceptable for general
    performance.
    
    So we only activate these when CCAN_STR_DEBUG is defined.

--- ccan/str/str.h
+++ ccan/str/str.h
@@ -1,7 +1,9 @@
 #ifndef CCAN_STR_H
 #define CCAN_STR_H
+#include "config.h"
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 /**
  * streq - Are two strings equal?
@@ -68,5 +70,66 @@ static inline bool strends(const char *str, const char *postfix)
  *      i = strcount("aaa aaa", "aa"); // i = 2;
  */
 size_t strcount(const char *haystack, const char *needle);
+
+#include <ccan/str/str_debug.h>
+
+/* These checks force things out of line, hence they are under DEBUG. */
+#ifdef CCAN_STR_DEBUG
+#include <ccan/build_assert/build_assert.h>
+
+/* These are commonly misused: they take -1 or an *unsigned* char value. */
+#undef isalnum
+#undef isalpha
+#undef isascii
+#undef isblank
+#undef iscntrl
+#undef isdigit
+#undef isgraph
+#undef islower
+#undef isprint
+#undef ispunct
+#undef isspace
+#undef isupper
+#undef isxdigit
+
+/* You can use a char if char is unsigned. */
+#if HAVE_BUILTIN_TYPES_COMPATIBLE_P && HAVE_TYPEOF
+#define str_check_arg_(i)						\
+	((i) + BUILD_ASSERT_OR_ZERO(!__builtin_types_compatible_p(typeof(i), \
+								  char)	\
+				    || (char)255 > 0))
+#else
+#define str_check_arg_(i) (i)
+#endif
+
+#define isalnum(i) str_isalnum(str_check_arg_(i))
+#define isalpha(i) str_isalpha(str_check_arg_(i))
+#define isascii(i) str_isascii(str_check_arg_(i))
+#define isblank(i) str_isblank(str_check_arg_(i))
+#define iscntrl(i) str_iscntrl(str_check_arg_(i))
+#define isdigit(i) str_isdigit(str_check_arg_(i))
+#define isgraph(i) str_isgraph(str_check_arg_(i))
+#define islower(i) str_islower(str_check_arg_(i))
+#define isprint(i) str_isprint(str_check_arg_(i))
+#define ispunct(i) str_ispunct(str_check_arg_(i))
+#define isspace(i) str_isspace(str_check_arg_(i))
+#define isupper(i) str_isupper(str_check_arg_(i))
+#define isxdigit(i) str_isxdigit(str_check_arg_(i))
+
+#if HAVE_TYPEOF
+/* With GNU magic, we can make const-respecting standard string functions. */
+#undef strstr
+#undef strchr
+#undef strrchr
+
+/* + 0 is needed to decay array into pointer. */
+#define strstr(haystack, needle)					\
+	((typeof((haystack) + 0))str_strstr((haystack), (needle)))
+#define strchr(haystack, c)					\
+	((typeof((haystack) + 0))str_strchr((haystack), (c)))
+#define strrchr(haystack, c)					\
+	((typeof((haystack) + 0))str_strrchr((haystack), (c)))
+#endif
+#endif /* CCAN_STR_DEBUG */
 
 #endif /* CCAN_STR_H */
---
+++ ccan/str/test/compile_fail-isxdigit.c
@@ -0,0 +1,23 @@
+#define CCAN_STR_DEBUG 1
+#include <ccan/str/str.h>
+
+int main(int argc, char *argv[])
+{
+#ifdef FAIL
+#if !HAVE_BUILTIN_TYPES_COMPATIBLE_P || !HAVE_TYPEOF
+#error We need typeof to check isxdigit.
+#endif
+	char
+#else
+	unsigned char
+#endif
+		c = argv[0][0];
+
+#ifdef FAIL
+	/* Fake fail on unsigned char platforms. */
+	c = 255;
+	BUILD_ASSERT(c < 0);
+#endif
+
+	return isxdigit(c);
+}
