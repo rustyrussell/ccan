b3ae89992ecaffed31dcc2e912539c289478801a
Rusty Russell <rusty@rustcorp.com.au>
2011-08-31 15:39:17 +0930

    tdb2: test: Speed up run-01-new_database.
    Don't run tdb_check with failtest on, since it is very slow.  Do the
    tdb_check w/ failtest in a new test, but skip MMAP suppression which
    doesn't add much and slows down valgrind a lot.
    
    Before this change run-01-new_database took 40 seconds (under
    valgrind), after it takes 8 seconds, and run-12-check takes 3
    seconds).

--- ccan/tdb2/test/run-12-check.c
+++ ccan/tdb2/test/run-12-check.c
@@ -1,3 +1,4 @@
+#include <ccan/tdb2/private.h>
 #include <ccan/failtest/failtest_override.h>
 #include "tdb2-source.h"
 #include <ccan/tap/tap.h>
@@ -20,31 +21,41 @@ int main(int argc, char *argv[])
 {
 	unsigned int i;
 	struct tdb_context *tdb;
-	int flags[] = { TDB_INTERNAL, TDB_DEFAULT, TDB_NOMMAP,
-			TDB_INTERNAL|TDB_CONVERT, TDB_CONVERT, 
-			TDB_NOMMAP|TDB_CONVERT,
-			TDB_INTERNAL|TDB_VERSION1, TDB_VERSION1,
-			TDB_NOMMAP|TDB_VERSION1,
+	int flags[] = { TDB_INTERNAL,
+			TDB_INTERNAL|TDB_CONVERT,
+			TDB_CONVERT,
+			TDB_INTERNAL|TDB_VERSION1,
+			TDB_VERSION1,
 			TDB_INTERNAL|TDB_CONVERT|TDB_VERSION1,
-			TDB_CONVERT|TDB_VERSION1,
-			TDB_NOMMAP|TDB_CONVERT|TDB_VERSION1 };
+			TDB_CONVERT|TDB_VERSION1 };
+	struct tdb_data key = tdb_mkdata("key", 3);
+	struct tdb_data data = tdb_mkdata("data", 4);
 
 	failtest_init(argc, argv);
 	failtest_hook = suppress_failure;
 	failtest_exit_check = exit_check_log;
-	plan_tests(sizeof(flags) / sizeof(flags[0]) * 3);
-	for (i = 0; i < sizeof(flags) / sizeof(flags[0]); i++) {
-		tdb = tdb_open("run-new_database.tdb", flags[i],
-			       O_RDWR|O_CREAT|O_TRUNC, 0600, &tap_log_attr);
-		if (!ok1(tdb))
-			failtest_exit(exit_status());
 
-		failtest_suppress = true;
-		ok1(tdb_check(tdb, NULL, NULL) == 0);
+	failtest_suppress = true;
+	plan_tests(sizeof(flags) / sizeof(flags[0]) * 3 + 1);
+	for (i = 0; i < sizeof(flags) / sizeof(flags[0]); i++) {
+		tdb = tdb_open("run-10-simple-store.tdb", flags[i],
+			       O_RDWR|O_CREAT|O_TRUNC, 0600, &tap_log_attr);
+		ok1(tdb);
+		ok1(tdb_store(tdb, key, data, TDB_INSERT) == 0);
+
+		/* This is what we really want to test: tdb_check(). */
 		failtest_suppress = false;
+		if (!ok1(tdb_check(tdb, NULL, NULL) == 0))
+			goto fail;
+		failtest_suppress = true;
+
 		tdb_close(tdb);
-		if (!ok1(tap_log_messages == 0))
-			break;
 	}
+	ok1(tap_log_messages == 0);
+	failtest_exit(exit_status());
+
+fail:
+	failtest_suppress = true;
+	tdb_close(tdb);
 	failtest_exit(exit_status());
 }
