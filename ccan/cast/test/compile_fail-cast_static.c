6569a707d169a629e25e10710c760c8dc84525c7
Rusty Russell <rusty@rustcorp.com.au>
2012-03-27 15:40:45 +1030

    cast: test/compile_fail-cast_static.c should fail without COMPOUND_LITERALS.
    It still gave a warning on gcc, because casting a char to a char* gives a warning.  Not so on sun CC.

--- ccan/cast/test/compile_fail-cast_static.c
+++ ccan/cast/test/compile_fail-cast_static.c
@@ -3,15 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
+	long c;
 #ifdef FAIL
 	char *
 #else
-	long
+	char
 #endif
 		x = 0;
 
-	c = cast_static(char, x);
+	c = cast_static(long, x);
 	(void) c; /* Suppress unused-but-set-variable warning. */
 	return 0;
 }
+
+#ifdef FAIL
+#if !HAVE_COMPOUND_LITERALS
+#error "Unfortunately we don't fail if cast_static without compound literals"
+#endif
+#endif
