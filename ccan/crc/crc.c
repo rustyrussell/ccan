b7cbecb9b59bc59144272e6d98ddcb0da8783684
Rusty Russell <rusty@rustcorp.com.au>
2012-10-31 21:33:59 +1030

    crc: remove gratuitous trailing whitespace.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/crc/crc.c
+++ ccan/crc/crc.c
@@ -9,7 +9,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) 
+ * Software Foundation; either version 2 of the License, or (at your option)
  * any later version. */
 
 /* crc64 code taken from Jacksum version 1.7.0 - checksum utility in Java
