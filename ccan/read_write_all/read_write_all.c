060a7e6d4f9e0872bef2cc1a7b484e45ae3bf374
Rusty Russell <rusty@rustcorp.com.au>
2013-02-06 16:37:56 +1030

    read_write_all: simplify and add comments.
    Also remove _ prefix from idempotent wrapper.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/read_write_all/read_write_all.c
+++ ccan/read_write_all/read_write_all.c
@@ -3,29 +3,14 @@
 #include <unistd.h>
 #include <errno.h>
 
-bool write_all(int fd, const void *data, size_t size)
+/* GCC inlines this quite well on -O2. */
+static inline bool do_all(int fd, void *data, size_t size,
+			  ssize_t (*op)(int, void *, size_t))
 {
 	while (size) {
 		ssize_t done;
 
-		done = write(fd, data, size);
-		if (done < 0 && errno == EINTR)
-			continue;
-		if (done <= 0)
-			return false;
-		data = (const char *)data + done;
-		size -= done;
-	}
-
-	return true;
-}
-
-bool read_all(int fd, void *data, size_t size)
-{
-	while (size) {
-		ssize_t done;
-
-		done = read(fd, data, size);
+		done = op(fd, data, size);
 		if (done < 0 && errno == EINTR)
 			continue;
 		if (done <= 0)
@@ -37,3 +22,12 @@ bool read_all(int fd, void *data, size_t size)
 	return true;
 }
 
+bool write_all(int fd, const void *data, size_t size)
+{
+	return do_all(fd, (void *)data, size, (void *)write);
+}
+
+bool read_all(int fd, void *data, size_t size)
+{
+	return do_all(fd, data, size, read);
+}
--- ccan/read_write_all/read_write_all.h
+++ ccan/read_write_all/read_write_all.h
@@ -1,10 +1,38 @@
 /* Licensed under LGPLv2+ - see LICENSE file for details */
-#ifndef _CCAN_READ_WRITE_H
-#define _CCAN_READ_WRITE_H
+#ifndef CCAN_READ_WRITE_H
+#define CCAN_READ_WRITE_H
 #include <stddef.h>
 #include <stdbool.h>
 
+/**
+ * write_all - write all of the given data to an fd.
+ * @fd: the file descriptor to write to.
+ * @data: the data to write.
+ * @size: the amount of data to write (in bytes).
+ *
+ * This loops, catching EINTR from signals or short writes.  It returns true
+ * if everything was written, false (and sets errno) otherwise.
+ *
+ * Example:
+ *	if (!write_all(0, "SUCCESS", strlen("SUCCESS")))
+ *		exit(1);
+ */
 bool write_all(int fd, const void *data, size_t size);
+
+/**
+ * read_all - read all of the given data from an fd.
+ * @fd: the file descriptor to read from.
+ * @data: the buffer to read into.
+ * @size: the amount of data to read (in bytes).
+ *
+ * This loops, catching EINTR from signals or short reads.  It returns true
+ * if everything was read, false (and sets errno) otherwise.
+ *
+ * Example:
+ *	char buf[20];
+ *	if (!read_all(0, buf, 20))
+ *		exit(1);
+ */
 bool read_all(int fd, void *data, size_t size);
 
-#endif /* _CCAN_READ_WRITE_H */
+#endif /* CCAN_READ_WRITE_H */
