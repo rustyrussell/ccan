7ec5b8e06b2fd5fa98b1fcde1158c286d2d429d8
David Gibson <david@gibson.dropbear.id.au>
2014-09-10 00:22:52 +1000

    container_of: Add container_of_or_null()
    It's quite common to have a pointer which could be either a pointer to a
    structure member, or NULL.  This needs special casing with container_of(),
    or it will convert NULL into something strange.
    
    This patch adds container_of_or_null(), which will return NULL if passed
    (an appropriately typed) NULL, or the containining structure as
    container_of() otherwise.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/container_of/test/run.c
+++ ccan/container_of/test/run.c
@@ -12,9 +12,13 @@ int main(int argc, char *argv[])
 	int *intp = &foo.a;
 	char *charp = &foo.b;
 
-	plan_tests(8);
+	plan_tests(12);
 	ok1(container_of(intp, struct foo, a) == &foo);
 	ok1(container_of(charp, struct foo, b) == &foo);
+	ok1(container_of_or_null(intp, struct foo, a) == &foo);
+	ok1(container_of_or_null(charp, struct foo, b) == &foo);
+	ok1(container_of_or_null((int *)NULL, struct foo, a) == NULL);
+	ok1(container_of_or_null((char *)NULL, struct foo, b) == NULL);
 	ok1(container_of_var(intp, &foo, a) == &foo);
 	ok1(container_of_var(charp, &foo, b) == &foo);
 
