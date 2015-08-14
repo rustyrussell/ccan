eee52d39cf67431bb750eb806c0f522fe87d9669
Rusty Russell <rusty@rustcorp.com.au>
2015-08-14 09:54:40 +0930

    cpuid: fix test.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/cpuid/test/run.c
+++ ccan/cpuid/test/run.c
@@ -13,7 +13,7 @@ int main(void)
 	uint32_t info = CPUID_HIGHEST_EXTENDED_FUNCTION_SUPPORTED | CPUID_EXTENDED_L2_CACHE_FEATURES | CPUID_VIRT_PHYS_ADDR_SIZES
 			| CPUID_PROCINFO_AND_FEATUREBITS;
 	uint32_t featureset = CPUID_FEAT_ECX_ALL | CPUID_FEAT_EDX_ALL;
-	if (!cpuid_write_info(info, featureset, NULL)) {
+	if (!cpuid_write_info(info, featureset, stdout)) {
 		printf("Failed to write CPU information!\n");
 		return 1;
 	}
