088030222124fc906364f30dcdc945191ecd61e7
David Gibson <david@gibson.dropbear.id.au>
2015-08-18 16:17:02 -0700

    bytestring: Add rational comment to testcase
    Reviewing the previous patch it took me some time to work out what the
    purpose of the compile_fail-BYTESTRING-2.c test.  Add a comment to avoid
    that in future.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- ccan/bytestring/test/compile_fail-BYTESTRING-2.c
+++ ccan/bytestring/test/compile_fail-BYTESTRING-2.c
@@ -4,6 +4,12 @@
 
 #include <ccan/bytestring/bytestring.h>
 
+/*
+ * BYTESTRING() can only be used safely on a literal string (or,
+ * strictly, something whose size can be determined with ARRAY_SIZE().
+ * This checks that it correctly fails to compile if used on a
+ * non-array pointer.
+ */
 int main(int argc, char *argv[])
 {
 	struct bytestring bs;
