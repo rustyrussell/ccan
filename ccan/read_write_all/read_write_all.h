eb1f73590044a4ae7a50d1a9ded56343a873c88c
Vasil Dimov <vd@FreeBSD.org>
2019-12-11 21:40:46 +0100

    read_write_all: fix read_all() to always set errno on failure
    read(2) will return 0 to designate end-of-file and in this case it will
    not set errno, but read_all() will return false, leaving errno as it
    were before the call.
    
    An EOF is not an error for read(2) itself, but read_all() is supposed to
    read exactly the requested number of bytes (not less), so set errno to
    EBADMSG "Bad message" in this case.

--- ccan/read_write_all/read_write_all.h
+++ ccan/read_write_all/read_write_all.h
@@ -5,6 +5,14 @@
 #include <stdbool.h>
 
 bool write_all(int fd, const void *data, size_t size);
+
+/**
+ * Read `size` bytes from the file descriptor `fd` into `data`, retrying on
+ * transient errors.
+ * If `size` bytes cannot be read then false is returned and errno is set
+ * to indicate the error, in which case the contents of `data` is undefined.
+ * If EOF occurs before `size` bytes were read, then errno is set to EBADMSG.
+ */
 bool read_all(int fd, void *data, size_t size);
 
 #endif /* _CCAN_READ_WRITE_H */
