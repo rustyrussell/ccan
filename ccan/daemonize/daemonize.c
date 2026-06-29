98f6f3060f2c6452204f54be1b921d51da821742
Rusty Russell <rusty@rustcorp.com.au>
2026-06-29 14:00:31 +0930

    daemonize: fix race with SIGHUP.
    Say something respectful here about Stevens not being perfect.
    
    Reported-by: https://github.com/Maxime2
    Closes: https://github.com/rustyrussell/ccan/pull/25
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/daemonize/daemonize.c
+++ ccan/daemonize/daemonize.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 
 /* This code is based on Stevens' Advanced Programming in the UNIX
@@ -11,15 +12,43 @@
 bool daemonize(void)
 {
 	pid_t pid;
+	int err;
+	int errfd[2];
+
+	/* Create a pipe to wait for child's setsid */
+	if (pipe(errfd) != 0)
+		return false;
 
 	/* Separate from our parent via fork, so init inherits us. */
-	if ((pid = fork()) < 0)
+	if ((pid = fork()) < 0) {
+		err = errno;
+		close(errfd[0]);
+		close(errfd[1]);
+		errno = err;
 		return false;
-	/* use _exit() to avoid triggering atexit() processing */
-	if (pid != 0)
-		_exit(0);
+	}
+
+	if (pid != 0) {
+		/* Parent: wait for child to setsid, so it doesn't get
+		 * SIGHUP! */
+		close(errfd[1]);
+	again:
+		if (read(errfd[0], &err, sizeof(err)) != sizeof(err)) {
+			if (errno == EINTR)
+				goto again;
+			err = EIO;
+		} else if (err == 0) {
+			/* use _exit() to avoid triggering atexit() processing */
+			_exit(0);
+		}
+
+		close(errfd[0]);
+		errno = err;
+		return false;
+	}
 
 	/* Don't hold files open. */
+	close(errfd[0]);
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
@@ -27,19 +56,32 @@ bool daemonize(void)
 	/* Many routines write to stderr; that can cause chaos if used
 	 * for something else, so set it here. */
 	if (open("/dev/null", O_WRONLY) != 0)
-		return false;
+		goto child_fail;
 	if (dup2(0, STDERR_FILENO) != STDERR_FILENO)
-		return false;
+		goto child_fail;
 	close(0);
 
 	/* Session leader so ^C doesn't whack us. */
 	if (setsid() == (pid_t)-1)
-	        return false;
+		goto child_fail;
+
 	/* Move off any mount points we might be in. */
 	if (chdir("/") != 0)
-		return false;
+		goto child_fail;
 
 	/* Discard our parent's old-fashioned umask prejudices. */
 	umask(0);
+	err = 0;
+	/* If parent goes away early, that's not really our problem. */
+	if (write(errfd[1], &err, sizeof(err)) != sizeof(err))
+		;
+	close(errfd[1]);
 	return true;
+
+child_fail:
+	/* Parent will get errno, we will exit quietly */
+	err = errno;
+	if (write(errfd[1], &err, sizeof(err)) != sizeof(err))
+		;
+	_exit(err);
 }
